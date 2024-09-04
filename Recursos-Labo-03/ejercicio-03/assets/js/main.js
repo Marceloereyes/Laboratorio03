287b65d32d7562ccaed5e6b560713f989ad73054
Marcelo Reyes <00148023@uca.edu.sv>
2024-09-03 21:14:30 -0600

    Ejercicio3

--- Recursos-Labo-03/ejercicio-03/assets/js/main.js
+++ Recursos-Labo-03/ejercicio-03/assets/js/main.js
@@ -1,13 +1,33 @@
-const calculateFactorial = (/*recibe*/) => {
-    //Code
-}
+const calculateFactorial = (n) => {
+    let factorial = 1;
+    for (let i = 1; i <= n; i++) {
+        factorial *= i;
+    }
+    return factorial;
+};
 
 const requestNumber = () => {
-    //Code
-}
+    let number;
+    while (true) {
+        number = prompt("Introduce un número entero no negativo para calcular su factorial:");
+        if (number === null) {
+            return null;
+        }
+        number = parseInt(number, 10);
+        if (isNaN(number) || number < 0) {
+            alert("Número no válido. Tiene que ser un número no negativo :).");
+        } else {
+            return number;
+        }
+    }
+};
 
 const main = () => {
-    //Code
-}
+    let number = requestNumber();
+    if (number === null) return;
+
+    let resultado = calculateFactorial(number);
+    alert(`El factorial de ${number} (iterativo) es: ${resultado}`);
+};
 
 main();
